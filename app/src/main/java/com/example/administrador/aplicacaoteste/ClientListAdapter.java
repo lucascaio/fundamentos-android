7ed8e351d9e42a7b5fef4d6d4293063744108e69
Lucas Matos <lucas_caio@live.com>
2015-07-20 21:14:07 -0300

    Commit tardio

---
+++ app/src/main/java/com/example/administrador/aplicacaoteste/ClientListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.administrador.aplicacaoteste;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Administrador on 20/07/2015.
+ */
+public class ClientListAdapter extends BaseAdapter {
+    private List<Client> clientList;
+    private Activity context;
+
+    public ClientListAdapter(Activity context, List<Client> clientList) {
+
+        this.clientList = clientList;
+    }
+
+    @Override
+    public int getCount() {
+        return clientList.size();
+    }
+
+    @Override
+    public Client getItem(int position) {
+        return clientList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = context.getLayoutInflater().inflate(R.layout.client_list_item, parent, false);
+        Client client = getItem(position);
+
+        TextView textViewName = (TextView) view.findViewById(R.id.textViewName);
+        textViewName.setText(client.getName());
+
+        TextView textViewAge = (TextView) view.findViewById(R.id.textViewAge);
+        textViewAge.setText(client.getAge());
+        return view;
+    }
+}
--- app/src/main/java/com/example/administrador/aplicacaoteste/MainActivity.java
+++ app/src/main/java/com/example/administrador/aplicacaoteste/MainActivity.java
@@ -2,32 +2,39 @@ package com.example.administrador.aplicacaoteste;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = MainActivity.class.getSimpleName();
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG, "onCreate");
 
-        Button botao = (Button)findViewById(R.id.botaoMaroto);
-        CheckBox checkIndefinido = (CheckBox)findViewById(R.id.checkBoxIndefinido);
-        botao.setText("btnMaroto teste");
-        checkIndefinido.setText("Check Teste");
-        botao.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Toast.makeText(MainActivity.this, "Cacete de Agulha", Toast.LENGTH_LONG).show();
-            }
-        });
+        List<Client> clienteNames = getClients();
 
+        ListView listVireCliente = (ListView) findViewById(R.id.listViewCliente);
+        ClientListAdapter clientAdapter = new ClientListAdapter(MainActivity.this, clienteNames);
     }
+
+    private List<Client> getClients() {
+        List<Client> clients = new ArrayList<>();
+        Client lucas = new Client();
+        lucas.setName("Lucas");
+        lucas.setAge(23);
+        clients.add(lucas);
+
+        Client garca = new Client();
+        garca.setName("Garca");
+        garca.setAge(24);
+        clients.add(garca);
+
+        return clients;
+    }
+
+
 }
